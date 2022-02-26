eff88d1304245af2b1a6e0d542ce26006d825001
Alexandre Bourget <alex@streamingfast.io>
2022-02-26 16:56:44 -0500

    Cleanup

--- wasm/example-block/build.rs
+++ wasm/example-block/build.rs
@@ -3,15 +3,3 @@ fn main() -> Result<()> {
     prost_build::compile_protos(&["proto/codec_eth.proto"], &["src/"])?;
     Ok(())
 }
-
-// // use prost_build;
-
-// fn main() {
-//     tonic_build::configure()
-//         .build_server(false)
-//         //.out_dir("src/pb")
-//         .compile(
-//             &["proto/bstream.proto", "proto/codec_eth.proto"],
-//             &["proto/"],
-//         ).unwrap();
-// }
