889110287fc5d0daae4158546b084f59945ad85e
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-03-01 12:10:13 -0500

    Moving unsafe to where we actually need it

--- wasm-filters/pair_extractor/src/lib.rs
+++ wasm-filters/pair_extractor/src/lib.rs
@@ -1,8 +1,10 @@
 mod memory;
+mod proto;
 
 use std::convert::TryInto;
 use std::io::Cursor;
 use hex;
+use crate::proto::proto::decode;
 
 pub mod eth {
     include!(concat!(env!("OUT_DIR"), "/dfuse.ethereum.codec.v1.rs"));
@@ -21,14 +23,7 @@ extern "C" {
 pub extern "C" fn map(ptr: *mut u8, len: usize) {
     register_panic_hook();
 
-    unsafe {
-        let input_data = Vec::from_raw_parts(ptr, len, len);
-        let blk: eth::Block = ::prost::Message::decode(&mut Cursor::new(&input_data)).unwrap();
-        std::mem::forget(input_data); // otherwise tries to free that memory at the end and crashes
-
-
-	// Log comments with:
-        //let msg = format!("msg0"); println(msg.as_ptr(), msg.len());
+    let blk: eth::Block = decode(ptr, len);
 
 	let mut pairs = pcs::Pairs{pairs: vec![]};
 	for trx in blk.transaction_traces {
@@ -37,37 +32,44 @@ pub extern "C" fn map(ptr: *mut u8, len: usize) {
 	    }
 
 	    for log in trx.receipt.unwrap().logs {
-		let sig = hex::encode(&log.topics[0]);
-		let msg = format!("trx: 0x{} sig: {}", hex::encode(&trx.hash), &sig); println(msg.as_ptr(), msg.len());
-		
-		if sig != "0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9" {
-		    continue
-		}
+            let sig = hex::encode(&log.topics[0]);
+            let msg = format!("trx: 0x{} sig: {}", hex::encode(&trx.hash), &sig);
 
-		// topics[0] is the event signature
-		let pair_token0 = decode_address(&log.topics[1]);
-		let pair_token1 = decode_address(&log.topics[2]);
-		let pair_addr = decode_address(&log.data);
+            unsafe {
+                println(msg.as_ptr(), msg.len());
+            }
 
-		pairs.pairs.push(pcs::Pair{
-		    address: pair_addr.clone(),
-		    token0: pair_token0,
-		    token1: pair_token1,
-		    creation_transaction_id: hex::encode(&trx.hash),
-		    block_num: blk.number,
-		    log_ordinal: log.block_index as u64,
-		})
-	    }
+            if sig != "0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9" {
+                continue
+            }
+
+            // topics[0] is the event signature
+            let pair_token0 = decode_address(&log.topics[1]);
+            let pair_token1 = decode_address(&log.topics[2]);
+            let pair_addr = decode_address(&log.data);
+
+            pairs.pairs.push(pcs::Pair{
+                address: pair_addr.clone(),
+                token0: pair_token0,
+                token1: pair_token1,
+                creation_transaction_id: hex::encode(&trx.hash),
+                block_num: blk.number,
+                log_ordinal: log.block_index as u64
+                })
+            }
 	}
 
-        let mut out = Vec::<u8>::new();
-        ::prost::Message::encode(&pairs, &mut out).unwrap();
+    let mut out = Vec::<u8>::new();
+    ::prost::Message::encode(&pairs, &mut out).unwrap();
 
-        let out_len = out.len();
-        let ptr = out.as_ptr();
-        std::mem::forget(out); // to prevent a drop which would crash
+    let out_len = out.len();
+    let ptr = out.as_ptr();
+    std::mem::forget(out); // to prevent a drop which would crash
+
+    unsafe {
         output(ptr as *const u8, (out_len as i32).try_into().unwrap());
     }
+
 }
 
 fn decode_address(input: &Vec::<u8>) -> String {
