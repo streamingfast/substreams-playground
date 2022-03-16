d047bd0faabc89299ba0bf29d573621a72e38280
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-03-16 16:52:03 -0400

    fixing wasm code to behave correctly like native code

--- pcs-rust/src/lib.rs
+++ pcs-rust/src/lib.rs
@@ -1,5 +1,7 @@
 mod eth;
 mod pb;
+mod rpc;
+mod utils;
 
 use eth::{address_pretty, decode_string, decode_uint32};
 use hex;
@@ -9,8 +11,8 @@ use substreams::{log, proto, state};
 #[no_mangle]
 pub extern "C" fn map_pairs(block_ptr: *mut u8, block_len: usize) {
     substreams::register_panic_hook();
-    let blk: pb::eth::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
 
+    let blk: pb::eth::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
     let mut pairs = pb::pcs::Pairs { pairs: vec![] };
 
     let msg = format!(
@@ -30,20 +32,16 @@ pub extern "C" fn map_pairs(block_ptr: *mut u8, block_len: usize) {
         for log in trx.receipt.unwrap().logs {
             let sig = hex::encode(&log.topics[0]);
 
+            /* keccak value for PairCreated(address,address,address,uint256) */
             if sig != "0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9" {
                 continue;
             }
 
-            // topics[0] is the event signature
-            let pair_token0 = address_pretty(&log.topics[1]);
-            let pair_token1 = address_pretty(&log.topics[2]);
-            let pair_addr = address_pretty(&log.data.as_slice());
-
             pairs.pairs.push(pb::pcs::Pair {
-                address: pair_addr.to_string(),
-                token0: pair_token0.to_string(),
-                token1: pair_token1.to_string(),
-                creation_transaction_id: hex::encode(&trx.hash),
+                address: address_pretty(&log.data[12..32]),
+                erc20_token0: rpc::get_token(Vec::from(&log.topics[1][12..])),
+                erc20_token1: rpc::get_token(Vec::from(&log.topics[2][12..])),
+                creation_transaction_id: address_pretty(&trx.hash),
                 block_num: blk.number,
                 log_ordinal: log.block_index as u64,
             })
@@ -60,8 +58,14 @@ pub extern "C" fn build_pairs_state(pairs_ptr: *mut u8, pairs_len: usize) {
     let pairs: pb::pcs::Pairs = proto::decode_ptr(pairs_ptr, pairs_len).unwrap();
 
     for pair in pairs.pairs {
-        let key = format!("pair:{}", pair.address);
-        state::set(pair.log_ordinal as i64, key, proto::encode(&pair).unwrap());
+        state::set(pair.log_ordinal as i64,
+                   format!("pair:{}", pair.address),
+                   proto::encode(&pair).unwrap()
+        );
+        state::set(pair.log_ordinal as i64,
+                   format!("tokens:{}", utils::generate_tokens_key(pair.erc20_token0.unwrap().address, pair.erc20_token1.unwrap().address)),
+                    Vec::from(pair.address)
+        )
     }
 }
 
@@ -80,7 +84,8 @@ pub extern "C" fn map_reserves(block_ptr: *mut u8, block_len: usize, pairs_store
                 None => continue,
                 Some(pair_bytes) => {
                     let sig = hex::encode(&log.topics[0]);
-                    // Sync(uint112,uint112)
+
+                    /* keccak value for Sync(uint112,uint112) */
                     if sig != "1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1" {
                         continue;
                     }
@@ -137,32 +142,13 @@ pub extern "C" fn map_to_database(
 pub extern "C" fn block_to_tokens(block_ptr: *mut u8, block_len: usize) {
     substreams::register_panic_hook();
 
-    let decimals = hex::decode("313ce567").unwrap();
-    let name = hex::decode("06fdde03").unwrap();
-    let symbol = hex::decode("95d89b41").unwrap();
-
     let mut tokens = pb::tokens::Tokens { tokens: vec![] };
     let blk: pb::eth::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
 
     for trx in blk.transaction_traces {
         for call in trx.calls {
             if call.call_type == pb::eth::CallType::Create as i32 && !call.state_reverted {
-                let rpc_calls = substreams::pb::eth::RpcCalls {
-                    calls: vec![
-                        substreams::pb::eth::RpcCall {
-                            to_addr: Vec::from(call.address.clone()),
-                            method_signature: decimals.clone(),
-                        },
-                        substreams::pb::eth::RpcCall {
-                            to_addr: Vec::from(call.address.clone()),
-                            method_signature: name.clone(),
-                        },
-                        substreams::pb::eth::RpcCall {
-                            to_addr: Vec::from(call.address.clone()),
-                            method_signature: symbol.clone(),
-                        },
-                    ],
-                };
+                let rpc_calls = rpc::create_rpc_calls(call.clone().address);
 
                 let rpc_responses_marshalled: Vec<u8> =
                     substreams::rpc::eth_call(substreams::proto::encode(&rpc_calls).unwrap());
@@ -171,19 +157,15 @@ pub extern "C" fn block_to_tokens(block_ptr: *mut u8, block_len: usize) {
 
                 if rpc_responses_unmarshalled.responses[0].failed
                     || rpc_responses_unmarshalled.responses[1].failed
-                    || rpc_responses_unmarshalled.responses[2].failed
-                {
+                    || rpc_responses_unmarshalled.responses[2].failed {
                     continue;
                 };
 
                 if !(rpc_responses_unmarshalled.responses[1].raw.len() >= 96)
                     || rpc_responses_unmarshalled.responses[0].raw.len() != 32
-                    || !(rpc_responses_unmarshalled.responses[2].raw.len() >= 96)
-                {
+                    || !(rpc_responses_unmarshalled.responses[2].raw.len() >= 96) {
                     continue;
                 };
-                // TODO:
-                // * write a ERC20Token object in the store with those three responses
 
                 let decoded_address = address_pretty(&call.address);
                 let decoded_decimals = decode_uint32(rpc_responses_unmarshalled.responses[0].raw.as_ref());
@@ -216,3 +198,22 @@ pub extern "C" fn build_tokens_state(tokens_ptr: *mut u8, tokens_len: usize) {
         state::set(1, key, proto::encode(&token).unwrap()); //todo: what about the log ordinal
     }
 }
+
+// #[no_mangle]
+// pub extern "C" fn build_reserves_state(reserves: pb::pcs::Reserves, pairs_store_idx: u32) {
+//     for update in reserves.reserves {
+//         match state::get_last(pairs_store_idx, format!("pair:{}", update.pair_address)) {
+//             None => continue,
+//             Some(pair_bytes) => {
+//                 let pair: pb::pcs::Pair = proto::decode(pair_bytes).unwrap(); // unmarshall pair
+//                 let token0 = pair.token0;
+//                 let token1 = pair.token1;
+//
+//                 // state::set(pair.log_ordinal as i64, format!("price:{}:{}", token0, token1), reserve.);
+//                 // state::set(pair.log_ordinal as i64, format!("price:{}:{}", token0, token1), reserve.);
+//                 state::set(pair.log_ordinal as i64, format!("reserve:{}:{}", update.pair_address, token0), proto::encode(&update.reserve0).unwrap());
+//                 state::set(pair.log_ordinal as i64, format!("reserve:{}:{}", update.pair_address, token1), proto::encode(&update.reserve1).unwrap());
+//             }
+//         }
+//     }
+// }
