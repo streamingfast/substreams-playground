8c519aa5b0065b82d6e637ced302fcb181945f38
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-03-21 21:22:45 -0400

    add proto

--- pcs-rust/src/pb/pcs.types.v1.rs
+++ pcs-rust/src/pb/pcs.types.v1.rs
@@ -49,3 +49,147 @@ pub struct Reserve {
     #[prost(string, tag="6")]
     pub token1_price: std::string::String,
 }
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PcsBaseEvents {
+    #[prost(message, repeated, tag="1")]
+    pub pcs_base_event: ::std::vec::Vec<PcsBaseEvent>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PcsBaseEvent {
+    #[prost(string, tag="1")]
+    pub pair_address: std::string::String,
+    #[prost(string, tag="2")]
+    pub token0: std::string::String,
+    #[prost(string, tag="3")]
+    pub token1: std::string::String,
+    #[prost(string, tag="4")]
+    pub transaction_id: std::string::String,
+    #[prost(uint64, tag="5")]
+    pub timestamp: u64,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PcsEvent {
+    #[prost(oneof="pcs_event::Event", tags="1, 2, 3, 4, 5, 6, 7")]
+    pub event: ::std::option::Option<pcs_event::Event>,
+}
+pub mod pcs_event {
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Event {
+        #[prost(message, tag="1")]
+        PairCreatedEvent(super::PairCreatedEvent),
+        #[prost(message, tag="2")]
+        PairApprovalEvent(super::PairApprovalEvent),
+        #[prost(message, tag="3")]
+        PairBurnEvent(super::PairBurnEvent),
+        #[prost(message, tag="4")]
+        PairMintEvent(super::PairMintEvent),
+        #[prost(message, tag="5")]
+        PairSwapEvent(super::PairSwapEvent),
+        #[prost(message, tag="6")]
+        PairSyncEvent(super::PairSyncEvent),
+        #[prost(message, tag="7")]
+        PairTransferEvent(super::PairTransferEvent),
+    }
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PairCreatedEvent {
+    #[prost(bytes, tag="1")]
+    pub log_address: std::vec::Vec<u8>,
+    #[prost(uint64, tag="2")]
+    pub log_index: u64,
+    #[prost(bytes, tag="3")]
+    pub token0: std::vec::Vec<u8>,
+    #[prost(bytes, tag="4")]
+    pub token1: std::vec::Vec<u8>,
+    #[prost(bytes, tag="5")]
+    pub pair: std::vec::Vec<u8>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PairApprovalEvent {
+    #[prost(bytes, tag="1")]
+    pub log_address: std::vec::Vec<u8>,
+    #[prost(uint64, tag="2")]
+    pub log_index: u64,
+    #[prost(bytes, tag="3")]
+    pub owner: std::vec::Vec<u8>,
+    #[prost(bytes, tag="4")]
+    pub spender: std::vec::Vec<u8>,
+    /// bigInt
+    #[prost(bytes, tag="5")]
+    pub value: std::vec::Vec<u8>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PairBurnEvent {
+    #[prost(bytes, tag="1")]
+    pub log_address: std::vec::Vec<u8>,
+    #[prost(uint64, tag="2")]
+    pub log_index: u64,
+    #[prost(bytes, tag="3")]
+    pub sender: std::vec::Vec<u8>,
+    /// bigInt
+    #[prost(bytes, tag="4")]
+    pub amount0: std::vec::Vec<u8>,
+    /// bigInt
+    #[prost(bytes, tag="5")]
+    pub amount1: std::vec::Vec<u8>,
+    #[prost(bytes, tag="6")]
+    pub to: std::vec::Vec<u8>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PairMintEvent {
+    #[prost(bytes, tag="1")]
+    pub log_address: std::vec::Vec<u8>,
+    #[prost(uint64, tag="2")]
+    pub log_index: u64,
+    #[prost(bytes, tag="3")]
+    pub sender: std::vec::Vec<u8>,
+    /// bigInt
+    #[prost(bytes, tag="4")]
+    pub amount0: std::vec::Vec<u8>,
+    /// bigInt
+    #[prost(bytes, tag="5")]
+    pub amount1: std::vec::Vec<u8>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PairSwapEvent {
+    #[prost(bytes, tag="1")]
+    pub log_address: std::vec::Vec<u8>,
+    #[prost(uint64, tag="2")]
+    pub log_index: u64,
+    #[prost(bytes, tag="3")]
+    pub sender: std::vec::Vec<u8>,
+    #[prost(bytes, tag="4")]
+    pub amount0_in: std::vec::Vec<u8>,
+    #[prost(bytes, tag="5")]
+    pub amount1_in: std::vec::Vec<u8>,
+    #[prost(bytes, tag="6")]
+    pub amount0_out: std::vec::Vec<u8>,
+    #[prost(bytes, tag="7")]
+    pub amount1_out: std::vec::Vec<u8>,
+    #[prost(bytes, tag="8")]
+    pub to: std::vec::Vec<u8>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PairSyncEvent {
+    #[prost(bytes, tag="1")]
+    pub log_address: std::vec::Vec<u8>,
+    #[prost(uint64, tag="2")]
+    pub log_index: u64,
+    #[prost(bytes, tag="3")]
+    pub reserve0: std::vec::Vec<u8>,
+    #[prost(bytes, tag="4")]
+    pub reserve1: std::vec::Vec<u8>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PairTransferEvent {
+    #[prost(bytes, tag="1")]
+    pub log_address: std::vec::Vec<u8>,
+    #[prost(uint64, tag="2")]
+    pub log_index: u64,
+    #[prost(bytes, tag="3")]
+    pub from: std::vec::Vec<u8>,
+    #[prost(bytes, tag="4")]
+    pub to: std::vec::Vec<u8>,
+    #[prost(bytes, tag="5")]
+    pub value: std::vec::Vec<u8>,
+}
