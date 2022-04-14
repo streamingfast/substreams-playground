63e59133e19443a2c2f3bc3f8ecf074d258aca07
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-04-14 08:51:25 -0400

    update db to reduce noise and adding to_string to macro field_from_strings

--- pcs-rust/src/macros.rs
+++ pcs-rust/src/macros.rs
@@ -25,8 +25,8 @@ macro_rules! field_from_strings {
     ($name:expr, $value:expr) => {
         Field {
             name: $name.to_string(),
-            new_value: String::from_utf8_lossy($value.new_value.as_slice()),
-            old_value: String::from_utf8_lossy($value.old_value.as_slice()),
+            new_value: String::from_utf8_lossy($value.new_value.as_slice()).to_string(),
+            old_value: String::from_utf8_lossy($value.old_value.as_slice()).to_string(),
         }
     };
 }
