4b12d136a6759bc6bdb9f858ea7144fc2acaacd9
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-05-03 12:08:33 -0400

    creating client-examples folder

--- client-examples/rust-client/build.rs
+++ client-examples/rust-client/build.rs
@@ -3,7 +3,7 @@ fn main() {
         .out_dir("src/pb")
         .compile(
             &["sf/substreams/v1/substreams.proto", "tokens.proto"],
-            &["../../substreams/proto", "../eth-token/proto/"],
+            &["../../../substreams/proto", "../../eth-token/proto/"],
         )
         .unwrap();
 }
