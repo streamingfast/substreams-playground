c62524f20440b55d42932f691a29e9d0ed8947f9
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-08-16 18:15:07 -0400

    removing unused proto files

--- modules/uniswap/build.rs
+++ modules/uniswap/build.rs
@@ -3,8 +3,5 @@ use std::io::Result;
 fn main() -> Result<()> {
     let mut prost_build = prost_build::Config::new();
     prost_build.out_dir("./src/pb");
-    prost_build.compile_protos(
-        &["codec_eth.proto", "modules.proto"],
-        &["../../external-proto", "./proto/"],
-    )
+    prost_build.compile_protos(&["modules.proto"], &["./proto/"])
 }
