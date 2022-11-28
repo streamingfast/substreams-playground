2c604bb5b53da730543a733f84cd7b76423ac9d2
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-11-28 09:52:24 -0500

    bump eth-block-producers-v0.5.5 to use proto as outputs

--- modules/eth-block-producers/src/lib.rs
+++ modules/eth-block-producers/src/lib.rs
@@ -6,21 +6,21 @@ use substreams::{log, store, Hex}; // {hex, log, proto, store, Hex};
 use substreams_ethereum::pb::eth as ethpb;
 
 #[substreams::handlers::map]
-fn map_coinbase(blk: ethpb::v1::Block) -> Result<String, Error> {
+fn map_coinbase(blk: ethpb::v1::Block) -> Result<pb::block_producers::Coinbase, Error> {
     log::info!("patate".to_string());
     match blk.header {
         Some(h) => {
             let stringed = Hex(h.coinbase).to_string();
             log::info!(stringed.clone());
-            Ok(stringed)
+            Ok(pb::block_producers::Coinbase { address: stringed })
         }
         None => Err(Unexpected("no header in block".to_string())),
     }
 }
 
 #[substreams::handlers::store]
-fn store_coinbase(coinbase: String, store: store::StoreAddInt64) {
-    let key = coinbase;
+fn store_coinbase(coinbase: pb::block_producers::Coinbase, store: store::StoreAddInt64) {
+    let key = coinbase.address;
     store.add(1, key.clone(), 1);
     log::info!("added key: {}", key);
     //set(1, key, &proto::encode(&token).unwrap());
---
+++ modules/eth-block-producers/src/pb/mod.rs
@@ -0,0 +1,3 @@
+#[path = "./sf.block_producers.rs"]
+#[allow(dead_code)]
+pub mod block_producers;
